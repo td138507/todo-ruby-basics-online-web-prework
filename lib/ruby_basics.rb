75d295f2183666130f12a0ab6e811b8c7faaef5d
Taylor Doyle <tcd56@cornell.edu>
2019-04-09 01:32:20 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ division = 6
 end
 
 def assign_variable(value)
-  
+  value="Bob"
 end
 
-def argue
-  
+def argue(name="I'm right and you are wrong!")
+  puts "Hello #{name}"
+  "#{name}"
 end
 
-def greeting
-
+def greeting(name1,name2)
+  puts "#{name1}, #{name2}"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+ last_evaluated_value="expert"
 end
 
-def pizza_party
-
+def pizza_party(food="cheese")
+  "#{food}"
 end
